654c95b10a1014ada60d5331af350cc3f18ee0d8
iamsurjog <iamsurjog@gmail.com>
2025-09-18 13:46:10 +0530

    feat: added file based routing with csv files

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask
-from tasks_blueprint.routes import tasks_bp
+from flask import Flask, Blueprint, render_template
+from tasks_blueprint.routes import create_tasks_blueprint
 import os
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(tasks_bp)
 
-    # Create CSV if not exists
-    if not os.path.exists("tasks.csv"):
-        with open("tasks.csv", "w") as f:
-            f.write("person,task\n")
-    
+    path_dir = 'path'
+    if not os.path.exists(path_dir):
+        os.makedirs(path_dir)
+
+    for csv_file in os.listdir(path_dir):
+        if csv_file.endswith('.csv'):
+            blueprint_name = os.path.splitext(csv_file)[0]
+            tasks_bp = create_tasks_blueprint(blueprint_name, os.path.join(path_dir, csv_file))
+            app.register_blueprint(tasks_bp, url_prefix=f'/{blueprint_name}')
+
+    @app.route('/')
+    def home():
+        path_dir = 'path'
+        csv_files = [f for f in os.listdir(path_dir) if f.endswith('.csv')]
+        return render_template('home.html', csv_files=csv_files)
+
     return app
 
 if __name__ == "__main__":
+    from flask import render_template
     app = create_app()
     app.run(debug=True)
-
--- tasks_blueprint/routes.py
+++ tasks_blueprint/routes.py
@@ -1,86 +1,62 @@
-from flask import Blueprint, render_template, request, redirect, url_for, Flask
+from flask import Blueprint, render_template, request, redirect, url_for
 import csv
 
-tasks_bp = Blueprint('tasks', __name__)
+def create_tasks_blueprint(blueprint_name, csv_file):
+    tasks_bp = Blueprint(blueprint_name, __name__)
 
+    def read_tasks(fname):
+        tasks = {}
+        with open(fname, newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                person = row['person']
+                if person not in tasks:
+                    tasks[person] = []
+                tasks[person].append([row['task'], row.get('complete', '')])
+        return tasks
 
-def read_tasks(fname):
-    tasks_Suki = []
-    tasks_Suj = []
-    with open(fname, newline='') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            if row['person'] == 'Suki':
-                tasks_Suki.append([row['task'], row['complete']])
-            elif row['person'] == 'Suj':
-                tasks_Suj.append([row['task'], row['complete']])
-    return tasks_Suki, tasks_Suj
+    def write_tasks(fname, tasks):
+        with open(fname, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['person', 'task', 'complete'])
+            for person, task_list in tasks.items():
+                for task in task_list:
+                    writer.writerow([person, task[0], task[1]])
 
-def write_tasks(fname, tasks_Suki, tasks_Suj):
-    with open(fname, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['person', 'task', 'complete'])
-        for task in tasks_Suki:
-            writer.writerow(['Suki', task[0], task[1]])
-        for task in tasks_Suj:
-            writer.writerow(['Suj', task[0], task[1]])
+    @tasks_bp.route("/")
+    def index():
+        tasks = read_tasks(csv_file)
+        persons = list(tasks.keys())
+        return render_template("index.html", tasks=tasks, persons=persons, blueprint_name=blueprint_name)
 
-@tasks_bp.route("/", methods=["GET", "POST"])
-def index():
-    tasks_Suki, tasks_Suj = read_tasks("tasks.csv")
-
-    # Adding a task
-    if request.method == "POST":
+    @tasks_bp.route("/add", methods=["POST"])
+    def add_task():
+        tasks = read_tasks(csv_file)
         person = request.form.get("person")
         task = request.form.get("task")
-        if person == "Suki":
-            tasks_Suki.append([task, ""])
-        elif person == "Suj":
-            tasks_Suj.append([task, ""])
-        write_tasks("tasks.csv", tasks_Suki, tasks_Suj)
-        return redirect(url_for('tasks.index'))
+        if person not in tasks:
+            tasks[person] = []
+        tasks[person].append([task, ""])
+        write_tasks(csv_file, tasks)
+        return redirect(url_for(f'{blueprint_name}.index'))
 
-    return render_template("index.html",
-                           tasks_Suki=tasks_Suki,
-                           tasks_Suj=tasks_Suj)
+    @tasks_bp.route("/complete/<person>/<int:task_id>")
+    def complete_task(person, task_id):
+        tasks = read_tasks(csv_file)
+        if person in tasks and 0 <= task_id < len(tasks[person]):
+            if tasks[person][task_id][1] == "":
+                tasks[person][task_id][1] = "checked"
+            else:
+                tasks[person][task_id][1] = ""
+            write_tasks(csv_file, tasks)
+        return redirect(url_for(f'{blueprint_name}.index'))
 
-# @tasks_bp.route("/hellothere", methods=["GET", "POST"])
-# def wat():
-#     if request.method == "POST":
-#         print(request.form.get("wat"))
-#     return "hello"
+    @tasks_bp.route("/delete/<person>/<int:task_id>")
+    def delete_task(person, task_id):
+        tasks = read_tasks(csv_file)
+        if person in tasks and 0 <= task_id < len(tasks[person]):
+            tasks[person].pop(task_id)
+            write_tasks(csv_file, tasks)
+        return redirect(url_for(f'{blueprint_name}.index'))
 
-@tasks_bp.route("/complete/<person>/<int:task_id>")
-def complete_task(person, task_id):
-    tasks_Suki, tasks_Suj = read_tasks("tasks.csv")
-
-    if person == "Suki":
-        if 0 <= task_id < len(tasks_Suki):
-            if tasks_Suki[task_id][1] == "":
-                tasks_Suki[task_id][1] = "checked"
-            elif tasks_Suki[task_id][1] == "checked":
-                tasks_Suki[task_id][1] = ""
-    elif person == "Suj":
-        if 0 <= task_id < len(tasks_Suj):
-            if tasks_Suj[task_id][1] == "":
-                tasks_Suj[task_id][1] = "checked"
-            elif tasks_Suj[task_id][1] == "checked":
-                tasks_Suj[task_id][1] = ""
-
-    write_tasks("tasks.csv", tasks_Suki, tasks_Suj)
-    return redirect(url_for('tasks.index'))
-
-@tasks_bp.route("/delete/<person>/<int:task_id>")
-def delete_task(person, task_id):
-    tasks_Suki, tasks_Suj = read_tasks("tasks.csv")
-
-    if person == "Suki":
-        if 0 <= task_id < len(tasks_Suki):
-            tasks_Suki.pop(task_id)
-    elif person == "Suj":
-        if 0 <= task_id < len(tasks_Suj):
-            tasks_Suj.pop(task_id)
-
-    write_tasks("tasks.csv", tasks_Suki, tasks_Suj)
-    return redirect(url_for('tasks.index'))
-
+    return tasks_bp
